703e3d0bdcdafa632f7c08352916acf7ef972b19
Iker Canut <ikermartincanut@gmail.com>
2020-03-29 18:15:44 -0300

    Autoevaluacion 0 - Programacion 1

--- 1 Año 1 Cuatrimestre/Programacion 1/Auto_Evaluacion_0.rkt
+++ 1 Año 1 Cuatrimestre/Programacion 1/Auto_Evaluacion_0.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname Auto_Evaluacion_0) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; EJERCICIO 1
 ; (2 + 3 * 4) / 5 + 6
